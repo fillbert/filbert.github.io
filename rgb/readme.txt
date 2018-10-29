83ccd6932e486141c1608d71ebefb581465830b9
FIll <Filbertby@gmail.com>
2018-10-29 19:28:33 +0300

    axaa

--- rgb/readme.txt
+++
@@ -1,3 +0,0 @@
-These files belong to the project https://github.com/maarten-pennings/WebUSB-LED
-Here is a web server that hosts them https://maarten-pennings.github.io/WebUSB-LED
-
--- rgb/rgb.js
+++ rgb/rgb.js
@@ -1,15 +1,14 @@
 (function() {
   'use strict';
- 
+
   document.addEventListener('DOMContentLoaded', event => {
     let connectButton = document.querySelector("#connect");
     let statusDisplay = document.querySelector('#status');
-    let ledCheckbox = document.querySelector('#led');
-    let butCheckbox = document.querySelector('#but');
+    let redSlider = document.querySelector('#red');
+    let greenSlider = document.querySelector('#green');
+    let blueSlider = document.querySelector('#blue');
     let port;
 
-    let ascii = (s => s.split('').map(c=>c.charCodeAt(0)) );
-    
     function connect() {
       port.connect().then(() => {
         statusDisplay.textContent = '';
@@ -17,10 +16,7 @@
 
         port.onReceive = data => {
           let textDecoder = new TextDecoder();
-          let msg= textDecoder.decode(data)
-          console.log(msg);
-          if( msg.indexOf("but=0")>=0 ) { butCheckbox.checked=false; ledCheckbox.checked=false }
-          if( msg.indexOf("but=1")>=0 ) { butCheckbox.checked=true;  ledCheckbox.checked=true  }
+          console.log(textDecoder.decode(data));
         }
         port.onReceiveError = error => {
           console.error(error);
@@ -30,21 +26,21 @@
       });
     }
 
-    function onUpdateLed() {
+    function onUpdate() {
       if (!port) {
         return;
       }
-      let cmd = ledCheckbox.checked ? "1" : "0" // Single char commands
-      port.send(Uint8Array.from( ascii(cmd)));
+
+      let view = new Uint8Array(3);
+      view[0] = parseInt(redSlider.value);
+      view[1] = parseInt(greenSlider.value);
+      view[2] = parseInt(blueSlider.value);
+      port.send(view);
     };
 
-    function onUpdateBut() {
-      butCheckbox.checked= !butCheckbox.checked
-      alert("Do not click the box; it is checked by pressing the button on the USB device.")
-    };
-
-    ledCheckbox.addEventListener('input', onUpdateLed);
-    butCheckbox.addEventListener('input', onUpdateBut);
+    redSlider.addEventListener('input', onUpdate);
+    greenSlider.addEventListener('input', onUpdate);
+    blueSlider.addEventListener('input', onUpdate);
 
     connectButton.addEventListener('click', function() {
       if (port) {
